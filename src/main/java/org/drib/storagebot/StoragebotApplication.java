9405b724c4969cf1ab4f30f0d3312c29df748a77
Vadym-mitin <gpnt84@gmail.com>
2025-03-06 16:53:03 +0200

    initial

---
+++ src/main/java/org/drib/storagebot/StoragebotApplication.java
@@ -0,0 +1,13 @@
+package org.drib.storagebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoragebotApplication.class, args);
+	}
+
+}
