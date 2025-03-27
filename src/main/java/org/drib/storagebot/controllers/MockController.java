8b40074045ed20c87f0cc2598d7a6b422d027b45
gpnt <gpnt84@gmail.com>
2025-03-27 19:25:13 +0200

    debug stuff

--- src/main/java/org/drib/storagebot/controllers/MockController.java
+++ src/main/java/org/drib/storagebot/controllers/MockController.java
@@ -1,26 +1,37 @@
 package org.drib.storagebot.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.drib.storagebot.util.SecretTokenUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @RestController
 @RequestMapping("/v1/mock")
 public class MockController {
 
+    private static String id;
+
+    public MockController() {
+        id = SecretTokenUtil.generateToken(10);
+    }
 
     @GetMapping("/whoami")
     public String whoAmI() {
         log.debug("/whoami was called");
-        return "You Are The Best!!";
+        return "I am the %s".formatted(id);
     }
 
     @GetMapping("/env")
     public Map<String, String> getEnvironmentVariables() {
-        return System.getenv();
+        Map<String, String> getenv = System.getenv();
+        String appServiceTelegramBotWebhookUrl = System.getenv("APP_SERVICE_TELEGRAM_BOT_WEBHOOK_URL");
+        getenv.put("App Service Telegram Bot Webhook Url Environment Variable", appServiceTelegramBotWebhookUrl);
+        return getenv;
     }
 }
